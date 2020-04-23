c11609d4eeea808306db67f427c2c67d42f3a9e3
Joni <jonibimbashi@gmail.com>
2020-04-23 19:36:10 +0200

    File upload functionality
    Now it is possible to upload files
    Renamed some folders
    Added new modules needed for visualizations
    Edited the home page so that to test the functionality
    
    Make sure to look at the changes and then how they conflict with your files in the local repository.
    
    If there are any questions about this commit or if there is any conflict with your current local cloned repository and you don't know what to do, write me on Discord
    
    If there isn't any conflict, be sure to pull first the files such that you update your local repository with the changes, to later commit your own changes.
    
    If any of this does not make sense, feel free to write to me on Discord

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var fileUpload = require('express-fileupload');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// default options
+app.use(fileUpload());
+
+app.post('/upload', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+  let uploadPath = __dirname + '/uploads/' + sampleFile.name;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath , function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,6 +58,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Demo' });
+  res.render('index', { title: 'Eye Tracking Visualization' });
 });
+
 
 module.exports = router;

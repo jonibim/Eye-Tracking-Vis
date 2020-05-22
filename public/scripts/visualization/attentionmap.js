1e98900dbd4b9238f399627fd9838fb81a77dd24
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-05-22 22:21:38 +0200

    convert attention map to D3.js

--- public/scripts/visualization/attentionmap.js
+++ public/scripts/visualization/attentionmap.js
@@ -1,5 +1,10 @@
 /**
  * Attention Map visualization
+ * @property svg - selection for the svg element which shows the attention map
+ * @property graphics - main group for svg elements
+ * @property zoom - d3 zoom object
+ * @property {number} resizeTimer - id for the timer which checks for changes in box size
+ * @property {HTMLImageElement} image - image used to get the natural width and height
  */
 class AttentionMap extends Visualization {
 
@@ -9,272 +14,94 @@ class AttentionMap extends Visualization {
     constructor(box) {
         super(box, 'Attention Map');
 
-        this.selecting = false;
-        this.selectionX = false;
-        this.selectionY = false;
-        this.selectionWidth = false;
-        this.selectionHeight = false;
+        this.width = this.box.inner.clientWidth;
+        this.height = this.box.inner.clientHeight;
 
-        // create an image element just to load the image from the server
-        this.img = new Image();
-        this.img.onload = () => {
-            this.ratio = 0;
-            this.draw();
-        }
+        this.svg = d3.select(this.box.inner)
+            .classed('smalldot ', true)
+            .append('svg')
+            .attr('width', this.width)
+            .attr('height', this.height);
 
-        // add canvas to document
-        this.canvas = document.createElement('canvas');
-        this.canvas.style.position = 'relative';
-        this.canvas.style.width = '100%';
-        this.canvas.style.height = '100%';
-        this.box.inner.appendChild(this.canvas);
+        this.graphics = this.svg.append('g');
 
-        // image for the color overlay
-        this.colorOverlay = document.createElement('canvas');
+        this.zoom = d3.zoom();
+        this.svg.call(
+            this.zoom.on('zoom', () => this.graphics.attr('transform', d3.event.transform))
+        );
 
-        // update area of interest
-        this.canvas.onmousedown = e => this.startSelection(e);
-        this.canvas.onmouseup = e => this.completeSelection(e);
-        this.canvas.onmouseleave = e => this.discardSelection(e);
-        this.canvas.onmousemove = e => this.checkSelection(e);
-
-        // make sure canvas resolution is the same as it's size
-        this.timer = setInterval(() => {
-            if (this.canvas.clientHeight !== this.canvas.height || this.canvas.clientWidth !== this.canvas.width) {
-                this.canvas.width = this.canvas.clientWidth;
-                this.canvas.height = this.canvas.clientHeight;
-                this.draw();
+        this.resizeTimer = setInterval(() => {
+            if (this.width !== this.box.inner.clientWidth || this.height !== this.box.inner.clientHeight) {
+                this.width = this.box.inner.clientWidth;
+                this.height = this.box.inner.clientHeight;
+                this.svg
+                    .attr('width', this.width)
+                    .attr('height', this.height);
             }
         }, 100);
 
-        // redraw when selected image changes
-        properties.onchange.set('attentionmap', () => {
-            this.img.src = properties.image ? '/testdataset/images/' + properties.image : '';
-        })
-        properties.getCurrentAOI().onchange.set('attentionmap', () => {
-            this.ratio = 0;
+        this.image = new Image();
+        this.image.onload = () => {
             this.draw();
-        })
+            this.center();
+        }
 
-        if (properties.image)
-            this.img.src = properties.image ? '/testdataset/images/' + properties.image : '';
+        properties.onchange.set('attentionmap', () => this.image.src = properties.image ? '/testdataset/images/' + properties.image : '')
+
+        if(properties.image)
+            this.image.src = '/testdataset/images/' + properties.image;
     }
 
     /**
      * Draws the attention map on the canvas
      */
-    draw() {
-        const context = this.canvas.getContext('2d');
+    draw(){
+        this.graphics.selectAll('*').remove();
 
-        // clear the canvas
-        context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        // return if no image is selected
         if (!properties.image)
             return;
 
-        // make sure aspect ration is maintained
-        let ratio = Math.min(this.canvas.width / this.img.naturalWidth, this.canvas.height / this.img.naturalHeight);
-        if (ratio !== this.ratio) {
-            this.ratio = ratio;
-            this.width = this.img.naturalWidth * this.ratio;
-            this.height = this.img.naturalHeight * this.ratio;
-            this.left = (this.canvas.width - this.width) / 2;
-            this.top = (this.canvas.height - this.height) / 2;
-            this.colorOverlay.width = this.width;
-            this.colorOverlay.height = this.height;
-            setTimeout(() => this.gatherColorRaster(), 0);
-        }
+        this.graphics.append('image')
+            .attr('href', this.image.src)
+            .attr('width', this.image.naturalWidth)
+            .attr('height', this.image.naturalHeight);
 
-        // draw the image
-        if (this.img) {
-            context.globalAlpha = 1;
-            context.drawImage(this.img, this.left, this.top, this.width, this.height);
-        }
-
-        // draw the color overlay onto the canvas
-        if (this.colorOverlay.width && this.colorOverlay.height) {
-            context.globalAlpha = 1;
-            context.drawImage(this.colorOverlay, this.left, this.top, this.width, this.height);
-        }
-
-        // draw selection
-        if (this.selecting) {
-            context.fillStyle = 'blue';
-            context.globalAlpha = 0.6;
-            context.fillRect(this.selectionX, this.selectionY, this.selectionWidth, this.selectionHeight);
-        }
-
-        this.drawScale();
-    }
-
-    /**
-     * Gets the attention per pixel of the image
-     */
-    gatherColorRaster() {
-        let raster = {};
-        let highestIndex = 0;
-
-        // get current image data
         const imageData = dataset.getImageData(properties.image);
-        const scanPaths = imageData.getScanPaths();
 
-        for (let scanPath of scanPaths) {
-            const points = scanPath.getPoints();
-            for (let point of points) {
-                let radius = Math.ceil(point.fixationDuration / 15 * this.ratio);
-                for (let x = -radius; x <= radius; x++) {
-                    for (let y = -radius; y <= radius; y++) {
-                        if (x * x + y * y <= radius * radius) {
-                            // the key is the position
-                            let key = Math.floor(point.x / this.img.naturalWidth * this.width + x) + ':' + Math.floor(point.y / this.img.naturalHeight * this.height + y);
-                            // index will be how important this pixel is
-                            let index = Math.pow(radius - Math.sqrt(x * x + y * y), 1 / 500);
-                            if (raster[key])
-                                raster[key] += index;
-                            else
-                                raster[key] = index;
+        let points = [];
+        imageData.scanpaths.forEach(path => points = points.concat(path.points));
 
-                            // keep track of the highest value
-                            if (raster[key] > highestIndex)
-                                highestIndex = raster[key];
-                        }
-                    }
-                }
-            }
-        }
+        let contours = d3.contourDensity()
+            .x(point => point.x)
+            .y(point => point.y)
+            .size([this.image.naturalWidth, this.image.naturalHeight])
+            .bandwidth(20)(points);
 
-        // draw to color overlay
-        const context = this.colorOverlay.getContext('2d');
-        for (let key in raster) {
-            let x = parseInt(key.substring(0, key.indexOf(':')));
-            let y = parseInt(key.substring(key.indexOf(':') + 1));
+        let color = d3.scalePow().domain([0,d3.max(contours, d => d.value)]).range(['#ffffff00',properties.getColorHex()]);
 
-            let color = properties.rgba;
-            let colorInverted = [255 - color[0], 255 - color[1], 255 - color[2], 1 - color[3]];
-
-            let index = Math.sin(raster[key] / highestIndex * Math.PI / 2);
-            let inverted = 1 - index;
-
-            let red = index * color[0] + inverted * colorInverted[0];
-            let green = index * color[1] + inverted * colorInverted[1];
-            let blue = index * color[2] + inverted * colorInverted[2];
-
-            let aoi = properties.getCurrentAOI();
-            if (!aoi.hasSelection || (x > aoi.left / this.img.naturalWidth * this.width && x < aoi.right / this.img.naturalWidth * this.width && y > aoi.top / this.img.naturalHeight * this.height && y < aoi.bottom / this.img.naturalHeight * this.height))
-                context.fillStyle = 'rgba(' + red + ',' + green + ',' + blue + ',' + index + ')';
-            else
-                context.fillStyle = 'rgba(' + index + ',' + index + ',' + index + ',' + (index / 255 * 0.6 + 0.1) + ')';
-            context.fillRect(x, y, 1, 1);
-        }
-
-        this.draw();
+        this.graphics.append('g')
+            .attr('fill', 'none')
+            .attr('stroke', 'none')
+            .attr('stroke-linejoin', 'round')
+            .selectAll('path')
+            .data(contours)
+            .enter().append('path')
+            .attr('fill', d => color(d.value))
+            .attr('stroke-width', (d, i) => i % 5 ? 0.25 : 1)
+            .attr('d', d3.geoPath());
     }
 
     /**
-     *
+     * Centers the attention map on the box
      */
-    drawScale() {
-        // TODO
+    center(){
+        let scale = Math.min(this.width / this.image.naturalWidth, this.height / this.image.naturalHeight);
+        this.graphics.call(this.zoom.translateTo, this.image.naturalWidth / 2, this.image.naturalHeight / 2);
+        this.graphics.call(this.zoom.scaleTo, scale);
     }
 
-    /**
-     * Updates the area of interest when dragging starts
-     * @param {MouseEvent} event
-     */
-    startSelection(event) {
-        if (this.selecting)
-            return;
-
-        let bounds = event.target.getBoundingClientRect();
-        let x = event.clientX - bounds.left;
-        let y = event.clientY - bounds.top;
-
-        if (x < this.left || x > this.left + this.width || y < this.top || y > this.top + this.height)
-            return;
-
-        this.selecting = true;
-        this.selectionX = x;
-        this.selectionY = y;
-        this.selectionWidth = 0;
-        this.selectionHeight = 0;
-    }
-
-    /**
-     * Updates the area of interest when dragging stops
-     * @param {MouseEvent} event
-     */
-    completeSelection(event) {
-        if (!this.selecting)
-            return;
-
-        let bounds = event.target.getBoundingClientRect();
-        let x = event.clientX - bounds.left;
-        let y = event.clientY - bounds.top;
-
-        this.selecting = false;
-        this.selectionWidth = x - this.selectionX;
-        this.selectionHeight = y - this.selectionY;
-
-        let left = Math.min(this.selectionX, this.selectionX + this.selectionWidth) - this.left;
-        let top = Math.min(this.selectionY, this.selectionY + this.selectionHeight) - this.top;
-        let right = Math.max(this.selectionX, this.selectionX + this.selectionWidth) - this.left;
-        let bottom = Math.max(this.selectionY, this.selectionY + this.selectionHeight) - this.top;
-
-        left = left / this.width * this.img.naturalWidth;
-        top = top / this.height * this.img.naturalHeight;
-        right = right / this.width * this.img.naturalWidth;
-        bottom = bottom / this.height * this.img.naturalHeight;
-
-        properties.getCurrentAOI().setSelection(left, top, right, bottom);
-
-        this.ratio = 0;
-        this.draw();
-    }
-
-    /**
-     * Updates the area of interest when dragging stops
-     * @param {MouseEvent} event
-     */
-    discardSelection(event) {
-        if (this.selecting) {
-            this.selecting = false;
-            this.draw();
-        }
-    }
-
-    /**
-     * Updates the area of interest when dragging stops
-     * @param {MouseEvent} event
-     */
-    checkSelection(event) {
-        if (!this.selecting)
-            return;
-
-        let bounds = event.target.getBoundingClientRect();
-        let x = event.clientX - bounds.left;
-        let y = event.clientY - bounds.top;
-
-        if (x < this.left || x > this.left + this.width || y < this.top || y > this.top + this.height) {
-            this.selecting = false;
-            this.draw();
-            return;
-        }
-
-        if (this.selectionWidth !== x - this.selectionX || this.selectionY !== y - this.selectionY) {
-            this.selectionWidth = x - this.selectionX;
-            this.selectionHeight = y - this.selectionY;
-            this.draw();
-        }
-    }
-
-    /**
-     * Called right before this visualization gets removed
-     */
     onRemoved() {
-        // stop the timer
-        if (this.timer)
-            clearInterval(this.timer);
+        if(this.resizeTimer)
+            clearInterval(this.resizeTimer);
     }
-
 }

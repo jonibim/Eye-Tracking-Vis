395a2ab691523213a3f6b1c85afadf426bffc2ec
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-06-05 13:55:52 +0200

    check for empty id

--- public/scripts/visualization/main.js
+++ public/scripts/visualization/main.js
@@ -65,7 +65,7 @@ window.onload = async () => {
     // get the url parameters
     let params = new URLSearchParams(window.location.search);
     // get dataset url for id or the default
-    const url = params.has('id') ? '/datasets/uploads/' + params.get('id') : '/datasets/default';
+    const url = params.has('id') && !!params.get('id').trim() ? '/datasets/uploads/' + params.get('id') : '/datasets/default';
     const request = fetch(url + '/data.json', { method: 'GET' });
     await request.then(response => response.arrayBuffer()).then(buffer => {
         let decoder = new TextDecoder("utf8");

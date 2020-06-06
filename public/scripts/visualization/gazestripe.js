1190c4a3ccdca2e548a964b7298c715469ebd837
Quint Bakens <quint.bakens@hotmail.com>
2020-06-06 21:12:21 +0200

    Major changes to the gazestripe visualization
    Implemented the ability to use different datasets, fixed bugs and implemented middle mouse button to pan as well as rewriting some parts of the script.

--- public/scripts/visualization/gazestripe.js
+++ public/scripts/visualization/gazestripe.js
@@ -1,180 +1,156 @@
-class GazeStripe extends Visualization {
-    
+class GazeStripe extends Visualization {   
     constructor(box) {
 
-		/**
-		 * the third parameter is used as a identifier for the HTML 
-		 * object so that it can be modified easily from the code
-		 * Such modicaitons can be as adding a loader 
-		 */
-        super(box, 'Gaze Stripe', 'gzviz')
+        /**
+         * the third parameter is used as a identifier for the HTML
+         * object so that it can be modified easily from the code
+         * Such modicaitons can be as adding a loader
+         */
+        super(box, 'Gaze Stripe');
 
-        let frameWidth = box.inner.clientWidth;
-        let frameHeight = box.inner.clientHeight;
+        this.frameWidth = this.box.inner.clientWidth;
+        this.frameHeight = this.box.inner.clientHeight;
 
-        this.img = new Image();
-        this.zoomValue = 50;
-        this.users;
-
-		let svg = d3.select(box.inner)
-			.classed('smalldot',true)
+        this.svg = d3.select(this.box.inner)
+            .classed('smalldot ', true)
             .append('svg')
-            .attr('width', frameWidth) // Full screen
-            .attr('height', frameHeight) // Full screen
-			.append('g')
-            .attr('transform', "translate(0 ,0)");
+            .attr('width', this.frameWidth)
+            .attr('height', this.frameHeight)
 
-        let usersx;
-        let usersy;
-        let shortestPath;
-        let longestTime; 
-       	let height;    
-        let data;
-        let drawing;
+        this.graphics = this.svg.append('g');
 
-        this.resizeTimer = setInterval(() => {
-            if (frameWidth !== this.box.inner.clientWidth || frameHeight !== this.box.inner.clientHeight) {
-            	scale();
-            	draw(this.img, this.zoomValue);
-            }
-        }, 100);
+        this.zoom = d3.zoom();
+        this.zoom.filter(() => (d3.event.type === 'mousedown' && d3.event.button === 1) || (d3.event.type === 'wheel' && d3.event.button === 0));
+        this.svg.call(
+            this.zoom.on('zoom', () => {
+                this.graphics.attr('transform', d3.event.transform);
+            })
+        );
 
-        properties.setListener('gazestripe', ['image','zoom','users'],() => {
-        	scale();
-			this.img = properties.image;
-			this.users = properties.users;
-			if (typeof properties.zoomValue !== 'undefined') {
-				this.zoomValue = properties.zoomValue;
-			}
-			if (drawing !== 1) {
-				drawing = 1;
-				loadData(this.img, this.zoomValue);
-			}
-        });
-
-        function scale() {
-        	
-        	frameWidth = box.inner.clientWidth;
-        	frameHeight = box.inner.clientHeight;
-
-        	svg
-        	.attr('width', frameWidth) // Full screen
-            .attr('height', frameHeight) // Full screen.attr('transform', "translate(0 ,0)");
-        	.attr('transform', "translate(0 ,0)");
+        this.image = new Image();
+        this.image.onload = () => {
+            this.draw();
         }
 
-	    function loadData(img, zoomValue) {
+        properties.setListener('gazestripe', ['image', 'users'], () => {
+            this.image.src = properties.image ? dataset.url + '/images/' + properties.image : '';
+            this.draw();
+        });
+        properties.setListener('gazestripe', 'zoom', () => {
+            this.draw();
+        });
+        properties.setListener('gazestripe', 'users', () => {
+            this.users = properties.users;
+            this.draw();
+        })
 
-	    	box.inner.clientWidth
-	    	usersx = {};
-	    	usersy = {};
-	    	shortestPath = Math.pow(10, 1000);
-	    	longestTime = {};
+        if (properties.image)
+            this.image.src = dataset.url + '/images/' + properties.image;
 
-	        d3.tsv(dataset.url + '/dataset.csv').then(function(data) {
-	            let dataByCity = d3.nest().key(function(data) {
-	                return data.StimuliName;
-	            }).entries(data);
-	            let dataByTimestamp = d3.nest().key(function(data) { 
-	                return data.user;
-	            }).entries(dataByCity);        
-	            data = dataByTimestamp[0]['values'];
+        this.resizeTimer = setInterval(() => {
+            if (this.frameWidth !== this.box.inner.clientWidth || this.frameHeight !== this.box.inner.clientHeight) {
+                this.scale();
+            }
+        }, 1000);
+    }
 
-	            processData(data, img);
-	            draw(img, zoomValue);
-	        })
-	    }
+    draw() {
 
-	    function processData(data, img) {
+        const imageData = dataset.getImageData(properties.image);
 
-	        let mapObj = {};
-	        for (var i = 0; i < data.length; i++) {
-	            mapObj[i] = Object.values(data)[i]['key'];   
-	        }           
-	                
-	        let map = Object.values(mapObj).indexOf((img));
-	        data[map]['values'].forEach(function(participant) {
-	        	if (properties.users.includes(participant['user'])) {
-		            let user = participant['user']
-		            if (typeof usersx[user] == 'undefined') {
-		                usersx[user] = [];
-		                usersy[user] = [];
-		                longestTime[user] = [];
-		            }
-		            usersx[user].push(participant['MappedFixationPointX']);
-		            usersy[user].push(participant['MappedFixationPointY']);
-		            longestTime[user].push(participant['Timestamp']);
-		        }
-	        });                
+        this.graphics.selectAll('*').remove();
 
-	        for (const key of Object.keys(usersx)) {
-	            if(usersx[key].length <= shortestPath) {
-	                shortestPath = usersx[key].length;
-	            }
-	        }
-	    }
+        if (!properties.image)
+            return;
 
-	    function draw(img, zoomValue) {
+        let usersx = {};
+        let usersy = {};
+        let shortestPath = Math.pow(10, 1000);
+        let longestTime = {};
 
-			if (!properties.image) {
-	    		return;
-	    	}
+        properties.users.forEach(function(participant) {
+            usersx[participant] = [];
+            usersy[participant] = [];
+            longestTime[participant] = [];
+        })
 
-	    	svg.selectAll("#gazestripeg").remove();
+        imageData['scanpaths'].forEach(function(user) {
+            let participant = user['person'];
+            if (properties.users.includes(participant)) {
+                user['points'].forEach(function(points) {
+                    usersx[participant].push(points['x']);
+                    usersy[participant].push(points['y']);
+                    longestTime[participant].push(points['time']);
+                })
+            }
+        })
+        
+        for (const key of Object.keys(usersx)) {
+            if(usersx[key].length <= shortestPath) {
+                shortestPath = usersx[key].length;
+            }
+        }
+        this.frameWidth = this.box.inner.clientWidth;
+        this.frameHeight = this.box.inner.clientHeight;
+        let width = (Math.floor(this.frameWidth / (shortestPath + 1))).toString();
+        let imgHeight = (Math.floor(this.frameWidth / (shortestPath + 1)));
+        let textHeight = ((Math.floor(this.frameWidth / shortestPath)) / 3).toString();
+        let fontsize1 = ((Math.floor(this.frameWidth / shortestPath)) / 3) * 0.7;
+        let fontsize2 = ((Math.floor(this.frameWidth / shortestPath)) / 3) * 0.7 * 0.7;
+        let height = Object.keys(usersx).length * (Number(this.imgHeight) + Number(this.textHeight));
+        let zoomValue = properties.zoomValue ? properties.zoomValue : 50;
 
-	        let width = (Math.floor(frameWidth / (shortestPath + 3))).toString();
-	        let imgHeight = width;
-	        let textHeight = ((Math.floor(frameWidth / shortestPath)) / 3).toString();
-	        let fontsize1 = textHeight * 0.7;
-	        let fontsize2 = fontsize1 * 0.7;
-	        let height = Object.keys(usersx).length * (Number(imgHeight) + Number(textHeight));
-	    	let counter = 0;
+        let counter = 0;
+        for (const key of Object.keys(usersx)) {
+            let divisor = usersx[key].length / shortestPath;
+            let timestamp = [0];
+            let imgLine = this.graphics.append('g').attr('id', 'gazestripeg');
+            let timeLine = this.graphics.append('g').attr('id', 'gazestripeg');
 
-	        for (const key of Object.keys(usersx)) {
-	        	let divisor = usersx[key].length / shortestPath;
-	 			let timestamp = [0];
-	        	let imgLine = svg.append('g').attr('id', 'gazestripeg');
-	        	let timeLine = svg.append('g').attr('id', 'gazestripeg');
+            imgLine.append('svg').attr('y', counter*(Number(imgHeight) + Number(textHeight)) + Number(textHeight)).attr('width', width).attr('height', imgHeight).attr('preserveAspectRatio', 'xMaxYMax meet').attr('viewBox', ('-' + fontsize1 + ' -' + (Number(fontsize2) * 2).toString() + ' ' + width + ' ' + width).toString()).append("text").text(key).attr('font-size', fontsize1);
+            timeLine.append('svg').attr('y', counter*(Number(imgHeight) + Number(textHeight)) + Number(imgHeight)).attr('width', width).attr('height', textHeight).attr('viewBox', '0 -' + fontsize2 + ' ' + width + ' ' + textHeight).append("text").text('Time (ms)').attr('font-size', fontsize2);
 
-				imgLine.append('svg').attr('y', counter*(Number(imgHeight) + Number(textHeight)) + Number(textHeight)).attr('width', width).attr('height', imgHeight).attr('preserveAspectRatio', 'xMaxYMax meet').attr('viewBox', ('-' + fontsize1 + ' -' + (Number(fontsize2) * 2).toString() + ' ' + width + ' ' + width).toString()).append("text").text(key).attr('font-size', fontsize1);
-	            timeLine.append('svg').attr('y', counter*(Number(imgHeight) + Number(textHeight)) + Number(imgHeight)).attr('width', width).attr('height', textHeight).attr('viewBox', '0 -' + fontsize2 + ' ' + width + ' ' + textHeight).append("text").text('Time (ms)').attr('font-size', fontsize2);
+            for (var i = 0; i < shortestPath; i++) {
+                let x = usersx[key][Math.round(divisor * i)];
+                let y = usersy[key][Math.round(divisor * i)];
+                imgLine
+                    .append('svg')
+                    .attr('x', (i + 1) * width)
+                    .attr('y', counter*(Number(textHeight) + Number(imgHeight)))
+                    .attr('width', width)
+                    .attr('height', imgHeight)
+                    .attr('viewBox', '' + (x-zoomValue) + ' ' + (y-zoomValue) + ' ' + (2*zoomValue) + ' ' + (2*zoomValue))
+                    .attr('preserveAspectRatio', 'xMinYMin slice')
+                    .append('image')
+                    .attr('xlink:href', this.image.src)
+                if (i >= 1) {
+                    let longestTimeIndex = Math.round(divisor * i);              
+                    timestamp.push(((longestTime[key][longestTimeIndex] -longestTime[key][longestTimeIndex-1]) + timestamp[timestamp.length - 1]));
+                }
+                timeLine
+                    .append('svg')
+                    .attr('x', (i + 1) * width)
+                    .attr('y', counter*(Number(imgHeight) + Number(textHeight)) + Number(imgHeight))
+                    .attr('width', width)
+                    .attr('height', textHeight)
+                    .attr('viewBox', (('-' + Number(fontsize2) * 1.5).toString() + ' -' + fontsize2 + ' ' + width + ' ' +  textHeight).toString())
+                    .append("text")
+                    .text(timestamp[timestamp.length - 1])
+                    .attr('font-size', fontsize2);
+            }
+            counter += 1;
+        }
+    }
 
-	        	for (var i = 0; i < shortestPath; i++) {
-	        		let x = usersx[key][Math.round(divisor * i)];
-	                let y = usersy[key][Math.round(divisor * i)];
-	        		imgLine
-	        			.append('svg')
-	        			.attr('x', (i + 1) * width)
-	        			.attr('y', counter*(Number(textHeight) + Number(imgHeight)))
-	        			.attr('width', width)
-	        			.attr('height', imgHeight)
-						.attr('viewBox', '' + (x-zoomValue) + ' ' + (y-zoomValue) + ' ' + (2*zoomValue) + ' ' + (2*zoomValue))
-	        			.attr('preserveAspectRatio', 'xMinYMin slice')
-	        			.append('image')
-	                    .attr('xlink:href', dataset.url + '/images/' + (img))
-	                if (i >= 1) {
-	                    let longestTimeIndex = Math.round(divisor * i);              
-	                    timestamp.push(((longestTime[key][longestTimeIndex] - longestTime[key][longestTimeIndex-1]) + timestamp[timestamp.length - 1]));
-	                }
-	                timeLine
-	                	.append('svg')
-	                	.attr('x', (i + 1) * width)
-	                	.attr('y', counter*(Number(imgHeight) + Number(textHeight)) + Number(imgHeight))
-	                	.attr('width', width)
-	                	.attr('height', textHeight)
-	                	.attr('viewBox', (('-' + Number(fontsize2) * 1.5).toString() + ' -' + fontsize2 + ' ' + width + ' ' +  textHeight).toString())
-	                	.append("text")
-	                	.text(timestamp[timestamp.length - 1])
-	                	.attr('font-size', fontsize2);
-	        	}
-	        	counter += 1;
-	        }
-	    	d3.select('svg').call(
-	    		d3.zoom()
-	    		.on("zoom", function () {
-	    	    svg.attr("transform", d3.event.transform)
-	    	}))
-			drawing = 0;
-		}
-	}
+    scale() {
+
+        this.frameWidth = this.box.inner.clientWidth;
+        this.frameHeight = this.box.inner.clientHeight;
+
+        this.svg
+        .attr('width', this.frameWidth) // Full screen
+        .attr('height', this.frameHeight) // Full screen.attr('transform', "translate(0 ,0)");
+        .attr('transform', "translate(0 ,0)");
+        this.draw();
+    }
 }

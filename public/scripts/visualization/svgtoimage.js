a58387f730f2be7ab8f176201d8d1a37c8c8dbf1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-06-06 16:19:36 +0200

    add 'createSVGImage' function

--- public/scripts/visualization/svgtoimage.js
+++ public/scripts/visualization/svgtoimage.js
@@ -3,98 +3,113 @@
  * @param {string} filename? - preferred name for the downloaded file
  * @return {HTMLImageElement}
  */
-function downloadSVG(svg, filename = 'image'){
-    parseImages(svg, () => {
-        let xml = new XMLSerializer().serializeToString(svg);
-        let url = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(xml);
+async function downloadSVG(svg, filename = 'image'){
+    await parseImages(svg);
+    let xml = new XMLSerializer().serializeToString(svg);
+    let url = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(xml);
 
-        let image = new Image();
+    let image = new Image();
 
-        image.onload = function () {
-            let canvas =  document.createElement('canvas');
-            canvas.width = image.naturalWidth * 5;
-            canvas.height = image.naturalHeight * 5;
-            canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
-            let data = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+    image.onload = function () {
+        let canvas =  document.createElement('canvas');
+        canvas.width = image.naturalWidth * 5;
+        canvas.height = image.naturalHeight * 5;
+        canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
+        let data = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
 
-            // create a button just to set the file name
-            let button = document.createElement('a');
-            button.style.display = 'none';
-            button.download = filename.endsWith('.png') ? filename : filename + '.png';
-            button.href = data;
-            // Firefox requires the button to be in the document
-            document.body.appendChild(button);
-            button.click();
-            document.body.removeChild(button);
-        };
+        // create a button just to set the file name
+        let button = document.createElement('a');
+        button.style.display = 'none';
+        button.download = filename.endsWith('.png') ? filename : filename + '.png';
+        button.href = data;
+        // Firefox requires the button to be in the document
+        document.body.appendChild(button);
+        button.click();
+        document.body.removeChild(button);
+    };
 
-        image.src = url;
-    });
+    image.src = url;
+}
+
+/**
+ * @param {SVGElement} svg
+ */
+async function createSVGImage(svg){
+    await parseImages(svg);
+
+    let xml = new XMLSerializer().serializeToString(svg);
+    let url = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(xml);
+
+    let image = new Image();
+    image.src = url;
+    return image;
 }
 
 /**
  * @param {SVGElement} svg
  * @param {function(svg: SVGElement)} callback
  */
-function parseImages(svg, callback){
-    let namespace = 'http://www.w3.org/1999/xlink';
+async function parseImages(svg){
+    return new Promise((resolve, reject) => {
+        let namespace = 'http://www.w3.org/1999/xlink';
 
-    // convert image to a dataURL
-    let parseImage = (image) => {
-        let img = new Image();
+        // convert image to a dataURL
+        let parseImage = (image) => {
+            let img = new Image();
 
-        img.onload = () => {
-            let canvas = document.createElement('canvas');
-            canvas.width = img.width;
-            canvas.height = img.height;
-            canvas.getContext('2d').drawImage(img, 0, 0);
-            image.setAttributeNS(namespace, 'href', canvas.toDataURL());
+            img.onload = () => {
+                let canvas = document.createElement('canvas');
+                canvas.width = img.width;
+                canvas.height = img.height;
+                canvas.getContext('2d').drawImage(img, 0, 0);
+                image.setAttributeNS(namespace, 'href', canvas.toDataURL());
 
-            left--;
-            if (left === 0)
-                callback(svg);
+                left--;
+                if (left === 0)
+                    resolve(svg);
+            };
+
+            img.src = image.getAttributeNS(namespace, 'href');
         };
 
-        img.src = image.getAttributeNS(namespace, 'href');
-    };
+        // convert svg to a dataURL
+        let parseSvg = (url, element) => {
+            let request = new XMLHttpRequest();
+            request.onload = () => {
+                let response = request.responseText || request.response;
+                let dataUrl = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(response);
+                element.setAttributeNS(namespace, 'href', dataUrl);
 
-    // convert svg to a dataURL
-    let parseSvg = (url, element) => {
-        let request = new XMLHttpRequest();
-        request.onload = () => {
-            let response = request.responseText || request.response;
-            let dataUrl = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(response);
-            element.setAttributeNS(namespace, 'href', dataUrl);
-
-            left--;
-            if(left === 0)
-                callback(svg);
+                left--;
+                if(left === 0)
+                    resolve(svg);
+            };
+            request.onerror = () => parseImage(element);
+            request.open('GET', url);
+            request.send();
         };
-        request.onerror = () => parseImage(element);
-        request.open('GET', url);
-        request.send();
-    };
 
-    let images = svg.querySelectorAll('image');
-    let left = images.length;
+        let images = svg.querySelectorAll('image');
+        let left = images.length;
 
-    // convert images which aren't in the correct format
-    for (let image of images) {
-        let href = image.getAttributeNS(namespace, 'href');
+        // convert images which aren't in the correct format
+        for (let image of images) {
+            let href = image.getAttributeNS(namespace, 'href');
 
-        if (href.indexOf('data:image') < 0){
-            if(href.indexOf('.svg') > 0)
-                parseSvg(href, image);
-            else
-                parseImage(image);
+            if (href.indexOf('data:image') < 0){
+                if(href.indexOf('.svg') > 0)
+                    parseSvg(href, image);
+                else
+                    parseImage(image);
+            }
+            else{
+                left--;
+                if (left === 0)
+                    resolve(svg);
+            }
         }
-        else{
-            left--;
-            if (left === 0)
-                callback(svg);
-        }
-    }
 
-    if (left === 0)
-        callback(svg);
+        if (left === 0)
+            resolve(svg);
+    })
 }
